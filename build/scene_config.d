9708ebc9d17d030b543d992b7f7435838ae0675f
Dave <digital.artist@hotmail.co.uk>
2024-12-06 07:44:52 +0000

    snapshot.

---
+++ build/scene_config.d
@@ -0,0 +1,16 @@
+build/scene_config.o: src/scene_config.c src/scene_config.h src/scene.h \
+ src/common.h src/vector.h src/stringy.h src/ray.h src/sphere.h \
+ src/light.h src/mesh.h src/animation.h src/json_parser.h \
+ src/xml_parser.h
+src/scene_config.h:
+src/scene.h:
+src/common.h:
+src/vector.h:
+src/stringy.h:
+src/ray.h:
+src/sphere.h:
+src/light.h:
+src/mesh.h:
+src/animation.h:
+src/json_parser.h:
+src/xml_parser.h:
