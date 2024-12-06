008164ce5fb6bc9330733e69acaca1ec19261ffa
digitalartist <37802495-digitalartist@users.noreply.replit.com>
2024-12-06 07:35:36 +0000

    Agent query: Is the scene rendering correctly with the new Vector2Double implementation and does the output show any remaining Vector2Double related errors?

--- build/vector.d
+++
@@ -1,9 +0,0 @@
-build/vector.o: src/vector.c src/vector.h src/stringy.h src/gc.h \
- src/aplib.h src/mesh.h src/common.h src/ray.h
-src/vector.h:
-src/stringy.h:
-src/gc.h:
-src/aplib.h:
-src/mesh.h:
-src/common.h:
-src/ray.h:
